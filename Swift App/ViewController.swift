73745557bc83f55fa2eeaf06b4dd2668f974c44b
IOS 7 First app commit <Mahoney@Alexs-Air.fios-router.home>
2017-04-12 11:54:41 -0400

    Changed to 20

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var newone: UILabel!
+    
+    var tapCount = 0
+    var PressmetapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    newone.text = "Hello there!"
+    print ("PUSH ME Button tapped") //Shows PUSH ME button was pressed
+    tapCount = tapCount + 1
+    print (tapCount) //tapcount allows you to see how many times a button was pressed
+    
+        if tapCount >= 20 {
+    newone.text = "You pressed this button 20 times"
+        }
+    }
+    
+    
+    @IBAction func PressMe(_ sender: Any) {
+    newone.text = "Buttons are awesome!"
+    print ("Press Me Button tapped") /*This button was asked to be placed in app per the exercise*/
+    PressmetapCount = PressmetapCount + 1
+    print (PressmetapCount)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
